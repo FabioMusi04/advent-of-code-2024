66a98e267655d35a2e223ca29c737a2caeb81178
FabioMusi04 <76118020+FabioMusi04@users.noreply.github.com>
2024-12-07 18:10:24 +0100

    feat: implement part 2

---
+++ 07/part2.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'input.txt');
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
+
+  const lines = data.trim().split('\r\n');
+  const array = [];
+  lines.forEach(line => {
+    const [testValue, equation] = line.split(':');
+    const numbers = equation.trim().split(' ').map(Number);
+    const found = canAchieveTarget(numbers, Number(testValue));
+    if (found) {
+      array.push(Number(testValue));
+    }
+  });
+
+  const sum = array.reduce((acc, cur) => acc + cur, 0);
+
+  console.log('Sum:', sum);
+});
+
+function canAchieveTarget(numbers, target) {
+  function helper(index, currentValue) {
+    if (index === numbers.length) {
+      return currentValue === target;
+    }
+
+    if (helper(index + 1, currentValue + numbers[index])) {
+      return true;
+    }
+
+    if (helper(index + 1, currentValue * numbers[index])) {
+      return true;
+    }
+
+    if (currentValue && helper(index + 1, Number(currentValue.toString() + numbers[index].toString()))) {
+      return true;
+    }
+
+    return false;
+  }
+
+  return helper(1, numbers[0])
+}
